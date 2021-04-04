2c1608e16c8e181185b67c4b3b50bcb0f7fbf2fa
Paul <roperp8@gmail.com>
2021-04-04 03:08:50 +0200

    dot Visualizer unfinished

---
+++ Audio Visualizer Project/Assets/Scripts/InitVisualizer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitVisualizer : MonoBehaviour
+{
+
+    public GameObject visualizerObject;
+    public int amount;
+    public Vector2 position;
+    public Vector2 offset = new Vector2(1, 0);
+
+    void Start()
+    {
+        for( int i = 0; i < amount; i++)
+        {
+            GameObject instantiatedObject = Instantiate(visualizerObject);
+            position += offset;
+            instantiatedObject.transform.position = position;
+
+        }   
+    }
+
+    
+}
